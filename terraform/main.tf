3c2c68f36ed3ff44fc9121050082b8ff9e7b1494
raj tayde <rajtayde0607@gmail.com>
2026-05-10 23:40:11 +0530

    feat: implement hybrid EKS node groups (On-Demand & Spot)
    - Added On-Demand node group for stable, critical workloads.
    - Added Spot node group for cost-optimized, non-critical scaling.
    - Applied custom labels ('workload=critical' and 'workload=non-critical') for pod affinity.
    - Configured t3.medium instances across multiple AZs for high availability.

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,26 +30,6 @@ resource "aws_subnet" "public_subnet_2" {
   }
 }
 
-# 3. Private Subnet (Safe Locker Room)
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.30.0/24"
-  availability_zone = "ap-south-1a"
-
-  tags = {
-    Name = "private-subnet"
-  }
-}
-
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.40.0/24"
-  availability_zone = "ap-south-1b"
-
-  tags = {
-    Name = "private-subnet-2"
-  }
-}
 
 # 4. Internet Gateway (Main Entry Gate)
 resource "aws_internet_gateway" "igw" {
@@ -84,12 +64,6 @@ resource "aws_route_table_association" "public_assoc_2" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# 6. Public Subnet ko Route Table se Jodna
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
 # eks implemented 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -101,21 +75,49 @@ module "eks" {
   subnet_ids = [
     aws_subnet.public_subnet_1.id,
     aws_subnet.public_subnet_2.id,
-    aws_subnet.private_subnet_1.id,
-    aws_subnet.private_subnet_2.id
   ]
 
   vpc_id = aws_vpc.main.id
 
   eks_managed_node_groups = {
-    default = {
+
+    on_demand = {
       instance_types = ["t3.medium"]
+
+      capacity_type = "ON_DEMAND"
 
       min_size     = 1
       max_size     = 2
       desired_size = 1
+
+      labels = {
+        workload = "critical"
+      }
+
+      tags = {
+        NodeGroup = "on_demand"
+      }
+    }
+
+    spot = {
+      instance_types = ["t3.medium"]
+
+      capacity_type = "SPOT"
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+
+      labels = {
+        workload = "non-critical"
+      }
+
+      tags = {
+        NodeGroup = "spot"
+      }
     }
   }
+
 
   cluster_addons = {
     coredns    = {}
