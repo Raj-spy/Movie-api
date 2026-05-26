290da62b8e49df88259af0c67b5c5a3758fee3b7
raj tayde <rajtayde0607@gmail.com>
2026-05-26 19:03:38 +0530

    feat(day-94): implement EBS CSI driver with dynamic persistent volume provisioning on EKS
    - enabled IRSA-based secure IAM access for EBS CSI driver
    - integrated aws-ebs-csi-driver addon into Terraform EKS setup
    - configured dynamic gp3 StorageClass provisioning
    - created and mounted PVC-backed persistent storage in Kubernetes
    - verified automatic EBS volume creation through CSI driver
    - tested persistent data survival across pod recreation
    - explored Stateful Kubernetes storage lifecycle and reclaim policies
    - debugged real EKS connectivity, kubeconfig, IAM auth, and RBAC issues
    - validated Kubernetes ↔ AWS storage orchestration through dynamic PV provisioning
    - differentiated node root EBS volumes from workload persistent storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,8 +72,11 @@ module "eks" {
   cluster_name    = "raj-eks-cluster"
   cluster_version = "1.30"
 
+  enable_irsa = true
+
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
+  
 
   subnet_ids = [
     aws_subnet.public_subnet_1.id,
@@ -126,9 +129,32 @@ module "eks" {
     coredns    = {}
     kube-proxy = {}
     vpc-cni    = {}
-  }
+    aws-ebs-csi-driver = {
+  service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
+   }
+}
 
   tags = {
     Environment = "dev"
   }
+}
+
+module "ebs_csi_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+
+  version = "~> 5.0"
+
+  create_role = true
+
+  role_name = "eks-ebs-csi-driver"
+
+  provider_url = module.eks.oidc_provider
+
+  role_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  ]
+
+  oidc_fully_qualified_subjects = [
+    "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+  ]
 }
